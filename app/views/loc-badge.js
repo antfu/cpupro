7570ec80d4816afc2c0170ddb30bbee932c7185c
Anthony Fu <github@antfu.me>
2024-05-20 13:00:03 +0200

    feat: support clicking loc to open editor

--- app/views/loc-badge.js
+++ app/views/loc-badge.js
@@ -3,5 +3,13 @@ discovery.view.define('loc-badge', {
     className: 'function-loc',
     data: 'function or $ | marker("function").object |? { ..., text: loc }',
     whenData: 'text',
-    content: 'html:text.split(/:/).join(`<span class="delim">:</span>`)'
+    content: 'html:text.split(/:/).join(`<span class="delim">:</span>`)',
+    postRender(el, _, data) {
+        if (data.module.path && data.module.path.match(/^\/|file:\/\/|[a-z]:/i)) {
+            const query = new URLSearchParams();
+            query.set('file', data.module.path);
+            el.target = '_blank';
+            el.href = 'https://localfile.link?' + query;
+        }
+    }
 }, { tag: false });
